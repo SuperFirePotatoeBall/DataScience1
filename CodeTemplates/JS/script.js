76273d3d084328ac29ab3c06f2ad202402b85373
SuperFirePotatoeBall <fireball68aj@gmail.com>
2023-11-09 11:14:00 -0700

    checkpoint, started JS coding

--- CodeTemplates/JS/script.js
+++ CodeTemplates/JS/script.js
@@ -1,1 +1,22 @@
-// JavaScript source code
+function button(){
+    var button = document.createElement("button");
+    button.innerHTML = "Dynamic Button";
+    button.id = "dynamicButton";
+
+    button.addEventListener("click", function() {
+        transition();
+    });
+    document.body.appendChild(button);
+}
+function transition(){
+    document.getElementById('fadeButton').addEventListener('click', function() {
+    var elementToFade = document.getElementById('elementToFade');
+    
+    // Toggle the opacity property to trigger the fade effect
+    if (elementToFade.style.opacity === '1') {
+        elementToFade.style.opacity = '0';
+    } else {
+        elementToFade.style.opacity = '1';
+    }
+});  
+}
