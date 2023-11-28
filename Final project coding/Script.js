114d52a0e6cb7bbf32a654136bc314b548c0eba9
SuperFirePotatoeBall <fireball68aj@gmail.com>
2023-11-28 11:33:00 -0700

    Basic design for final website has been chosen

--- Final project coding/Script.js
+++ Final project coding/Script.js
@@ -1,1 +1,27 @@
-// JavaScript source code
+window.addEventListener('scroll', handleScroll)
+
+const titleHeight = 1000;
+const questionHeight = 2000;
+
+
+
+function handleScroll (){
+    //Title
+    var title = document.getElementById("titleDiv");
+    if (getScrollDistance() > titleHeight){
+        title.style.opacity = 0;
+    } else if (getScrollDistance() <= titleHeight){
+        title.style.opacity = 100;
+    }
+    //Question
+    var question = document.getElementById("questionDiv");
+    if (getScrollDistance() > questionHeight || getScrollDistance() <= titleHeight){
+        question.style.opacity = 0;
+    } else if (getScrollDistance() <= questionHeight){
+        question.style.opacity = 100;
+    }
+}
+function getScrollDistance() {
+    var scrollDistance = document.documentElement.scrollTop || document.body.scrollTop;
+    return scrollDistance;
+}
